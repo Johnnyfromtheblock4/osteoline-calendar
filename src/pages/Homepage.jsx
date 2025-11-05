e180d1ba428f2b11a6cfd2f463823464e89b40dd
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-11-06 00:50:33 +0100

    [UPD] erased Navbar element

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,10 @@
 import CalendarApp from "../components/CalendarApp";
-import Navbar from "../components/Navbar";
 import "../styles/CalendarApp.css";
 
 const Homepage = () => {
   return (
     <div className="container-fluid">
       <CalendarApp />
-      <Navbar />
     </div>
   );
 };
