60a748acf0dd5091bdbafc051c14ee06db08bea5
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-11-08 18:11:58 +0100

    [UPD] ProfilePage

--- src/pages/HourCounter.jsx
+++ src/pages/HourCounter.jsx
@@ -1,10 +1,25 @@
+import "../styles/HourCounter.css";
 
 const HourCounter = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <h1 className="hour-counter-title my-4 text-center">Profilo</h1>
+      <div className="hour-counter-container container d-flex justify-content-center align-items-center mt-4">
+        <div className="hour-counter-box row">
+          <div className="col-12">
+            <h2 className="hour-counter-subtitle text-warning">
+              Conteggio ore mese corrente
+            </h2>
+            <p>Qui andra il conteggio delle ore del mese corrente</p>
+            <h2 className="hour-counter-subtitle text-warning">
+              Note
+            </h2>
+            <p>Qui andra textarea con realtivo aggiungi nelle note che va sotto a creare un card con data, titolo e nota con relativi tasti di modifica eliminazione e copia</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default HourCounter
+export default HourCounter;
