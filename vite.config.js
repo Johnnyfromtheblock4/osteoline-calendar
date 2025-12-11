1034f8ea724913f39f3e7d87f768148a2a43ba8f
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-12-11 02:44:10 +0100

    [UPD] manifest

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,21 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+
+  publicDir: "public",
+
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        sw: resolve(__dirname, "firebase-messaging-sw.js"),
+        sw: resolve(__dirname, "firebase-messaging-sw.js")
       },
       output: {
         entryFileNames: (chunk) => {
           if (chunk.name === "sw") return "firebase-messaging-sw.js";
           return "assets/[name]-[hash].js";
-        },
-      },
-    },
-  },
+        }
+      }
+    }
+  }
 });
